2be70c2fbbbfe5de4894ae77c8d43f54f087082d
搋兔 <naruto6404@hotmail.com>
2021-09-27 00:03:53 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div :class="[
+      'w-screen h-screen',
+      'flex flex-col',
+      'justify-center items-center',
+      'gap-5',
+  ]">
+    <input type="range" v-model="val" :min="min" :max="max" />
+    <ProgressBar :currentVal="val" :minVal="min" :maxVal="max" />
+  </div>
+</template>
+
+<script>
+import ProgressBar from './components/ProgressBar.vue'
+
+export default {
+  data() {
+    return {
+      min: 0,
+      max: 200,
+      val: 100,
+    }
+  },
+  components: {
+    ProgressBar,
+  }
+}
+</script>
---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,44 @@
+<template>
+  <div :class="[
+    'w-80 p-5',
+    'bg-gray-200',
+    'rounded-lg'
+  ]">
+    <div :class="[
+      'w-full h-2',
+      'bg-green-800',
+      'rounded-full'
+    ]">
+      <div 
+        :class="[
+          'w-1/3 h-full',
+          'bg-green-500',
+          'rounded-full'
+        ]"
+        :style="{
+          width: percent + '%'
+        }"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProgressBar",
+  props: ["minVal","maxVal","currentVal"],
+  data() {
+    return {
+      percent: 0,
+    }
+  },
+  mounted() {
+    this.percent = Number(this.currentVal*100/(this.maxVal-this.minVal))
+  },
+  watch: {
+    currentVal(newVal, oldVal) {
+      this.percent = Number(newVal*100/(this.maxVal-this.minVal))
+    }
+  }
+}
+</script>
